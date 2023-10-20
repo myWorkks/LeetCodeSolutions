4336c58a3ba21adffec1a148baac841afeda1918
nsgbharath <reddysbharath45@gmail.com>
2023-10-20 10:54:22 +0530

    solved 1047

--- src/com/leetCode/RemoveAllAdjacentDuplicatesInString1047.java
+++ src/com/leetCode/RemoveAllAdjacentDuplicatesInString1047.java
@@ -1,10 +1,36 @@
 package com.leetCode;
 
+import java.util.Stack;
+
+class Solution1047 {
+	public String removeDuplicates(String s) {
+		Stack<Character> stack = new Stack<Character>();
+		for (Character c : s.toCharArray()) {
+			if (stack.empty()) {
+				stack.push(c);
+			} else {
+				if (stack.peek().equals(c)) {
+					stack.pop();
+				} else {
+					stack.push(c);
+				}
+			}
+		}
+		String ss = "";
+		while (!stack.empty()) {
+			ss = stack.pop() + ss;
+		}
+		return ss;
+
+	}
+}
+
 public class RemoveAllAdjacentDuplicatesInString1047 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Solution1047 sol = new Solution1047();
+		String s = sol.removeDuplicates("azxxzy");
+		System.out.println(s);
 	}
 
 }
