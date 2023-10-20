2a6990deacf0f41f129eaf886ed5e8ee10f38004
nsgbharath <reddysbharath45@gmail.com>
2023-10-20 14:45:47 +0530

    solved 344

---
+++ src/com/leetCode/ContainsDuplicateIII220.java
@@ -0,0 +1,19 @@
+package com.leetCode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution220 {
+	public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+		return false;
+
+	}
+}
+
+public class ContainsDuplicateIII220 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/com/leetCode/ReverseString344.java
@@ -0,0 +1,25 @@
+package com.leetCode;
+
+class Solution344 {
+	public void reverseString(char[] s) {
+		int i=0;
+		int j=s.length-1;
+		while(i<j) {
+			char temp;
+			temp=s[i];
+			s[i]=s[j];
+			s[j]=temp;
+			
+		}
+		
+	}
+}
+
+public class ReverseString344 {
+
+	public static void main(String[] args) {
+		Solution344 s = new Solution344();
+		s.reverseString(new char[] { 'h', 'e', 'i', 'o' });
+	}
+
+}
